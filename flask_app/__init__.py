4559114bfa02c42d585f9310e8d8d85896a3e23e
Steve F <stevefrazee123@gmail.com>
2022-02-10 08:58:54 -0500

    move secret key to environment

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -2,12 +2,13 @@
 Contains global setup for app
 '''
 
+from flask_app.config.env import secret_key
 from flask_bcrypt import Bcrypt
 from flask import Flask
 
 # App setup
 app = Flask(__name__)
-app.secret_key = "-JaNcRfUjXn2r5u8x/A?D(G+KbPeSgVk"
+app.secret_key = secret_key
 
 # Bcrypt setup
 cryptor = Bcrypt(app)
